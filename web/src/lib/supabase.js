80c0ec1123d1416e3b565b0a997f7881b2f16cb4
Brandon Zuñiga <brandonjosuezunigahernandez@gmail.com>
2026-02-21 16:11:55 -0600

    Seguridad: 2da Configuracion de .gitignore

--- web/src/lib/supabase.js
+++ web/src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-
-const supabaseUrl = 'https://xudqdefuoxthkdrsmkqo.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1ZHFkZWZ1b3h0aGtkcnNta3FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MDg0MTYsImV4cCI6MjA4NzI4NDQxNn0.xtzsawDK1T6pg7onxVsQ_s5rrnzzTs1VngYsvhtGlPI'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
